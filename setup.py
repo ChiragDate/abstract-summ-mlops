235f33cfb306d90925c00d2ed38ea3ec6ae106a5
chirag <cdate022001@gmail.com>
2025-03-27 14:45:21 +0530

    Initial commit with DVC

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Project implements MLOps on an Encoder-Decoder model for research abstract generation.',
+    author='Chirag_Mohit',
+    license='',
+)
